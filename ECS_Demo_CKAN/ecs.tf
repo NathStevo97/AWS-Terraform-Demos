eebdb177fb56987cc62e0454d53c560d74335bd0
NathStevo97 <natstephenson15@gmail.com>
2023-03-10 19:55:28 +0000

    Temp Removal of CKAN ECS - To be put in branches

--- ECS_Demo_CKAN/ecs.tf
+++
@@ -1,6 +0,0 @@
-#----- ECS --------
-module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
-  cluster_name   = "ckan-ecs"
-}
-
--- ECS_Demo_CKAN/redis.tf
+++
@@ -1,7 +0,0 @@
-module "redis" {
-    source = "./modules/redis"
-    name = local.ckan.default.name
-    subnet_ids = module.vpc.private_subnets
-    vpc_id = module.vpc.vpc_id
-    allowed_cidr_blocks = [local.ckan.default.cidr]
-}
